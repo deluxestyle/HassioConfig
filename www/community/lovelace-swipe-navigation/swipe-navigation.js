2deedc08d39c425427aafbe8563faca1fa3305b9
deluxestyle <deluxestyle@googlemail.com>
2020-11-06 14:36:12 +0100

    update custom component

--- www/community/lovelace-swipe-navigation/swipe-navigation.js
+++ www/community/lovelace-swipe-navigation/swipe-navigation.js
@@ -1,215 +1,41 @@
-function swipeNavigation() {
-  let root = document.querySelector("home-assistant");
-  root = root && root.shadowRoot;
-  root = root && root.querySelector("home-assistant-main");
-  root = root && root.shadowRoot;
-  root = root && root.querySelector("app-drawer-layout partial-panel-resolver");
-  root = (root && root.shadowRoot) || root;
-  root = root && root.querySelector("ha-panel-lovelace");
-  root = root && root.shadowRoot;
-  root = root && root.querySelector("hui-root");
-  if (root == null) {
-    setTimeout(swipeNavigation, 300);
-    return;
-  }
-  const config = root.lovelace.config.swipe_nav || {};
-
-  let rtl =
-    (root.lovelace.config.custom_header &&
-      root.lovelace.config.custom_header.reverse_tab_direction) ||
-    document.querySelector("home-assistant").style.direction == "rtl"
-  let animate = config.animate !== undefined ? config.animate : "none";
-  let wrap = config.wrap !== undefined ? config.wrap : true;
-  let prevent_default =
-    config.prevent_default !== undefined ? config.prevent_default : false;
-  let swipe_amount =
-    config.swipe_amount !== undefined
-      ? config.swipe_amount / Math.pow(10, 2)
-      : 0.15;
-  let skip_hidden =
-    config.skip_hidden !== undefined ? config.skip_hidden : true;
-  let skip_tabs =
-    config.skip_tabs !== undefined
-      ? String(config.skip_tabs)
-          .replace(/\s+/g, "")
-          .split(",")
-          .map(function(item) {
-            return parseInt(item, 10);
-          })
-      : [];
-
-  const appLayout = root.shadowRoot.querySelector("ha-app-layout");
-  const view = appLayout.querySelector('[id="view"]');
-  const tabContainer = appLayout.querySelector("paper-tabs") || appLayout.querySelector("ha-tabs");
-  let xDown, yDown, xDiff, yDiff, activeTab, firstTab, lastTab, left;
-  let tabs = tabContainer ? Array.from(tabContainer.querySelectorAll("paper-tab")) : [];
-
-  if (tabContainer) {
-    appLayout.addEventListener("touchstart", handleTouchStart, { passive: true });
-    appLayout.addEventListener("touchmove", handleTouchMove, { passive: false });
-    appLayout.addEventListener("touchend", handleTouchEnd, { passive: true });
-  }
-
-  if (animate == "swipe") {
-    appLayout.style.overflow = "hidden";
-  }
-
-  function handleTouchStart(event) {
-    let ignored = [
-      "APP-HEADER",
-      "HA-SLIDER",
-      "SWIPE-CARD",
-      "HUI-MAP-CARD",
-      "ROUND-SLIDER",
-      "XIAOMI-VACUUM-MAP-CARD",
-      "HA-SIDEBAR"
-    ];
-    if (typeof event.path == "object") {
-      for (let element of event.path) {
-        if (element.nodeName == "HUI-VIEW") break;
-        else if (ignored.indexOf(element.nodeName) > -1) return;
-      }
-    }
-    xDown = event.touches[0].clientX;
-    yDown = event.touches[0].clientY;
-    if (!lastTab) filterTabs();
-    activeTab = tabs.indexOf(tabContainer.querySelector(".iron-selected"));
-  }
-
-  function handleTouchMove(event) {
-    if (xDown && yDown) {
-      xDiff = xDown - event.touches[0].clientX;
-      yDiff = yDown - event.touches[0].clientY;
-      if (Math.abs(xDiff) > Math.abs(yDiff) && prevent_default) {
-        event.preventDefault();
-      }
-    }
-  }
-
-  function handleTouchEnd() {
-    if (activeTab < 0 || Math.abs(xDiff) < Math.abs(yDiff)) {
-      xDown = yDown = xDiff = yDiff = null;
-      return;
-    }
-    if (rtl) {
-      xDiff = -xDiff;
-    }
-    if (xDiff > Math.abs(screen.width * swipe_amount)) {
-      left = false;
-      activeTab == tabs.length - 1 ? click(firstTab) : click(activeTab + 1);
-    } else if (xDiff < -Math.abs(screen.width * swipe_amount)) {
-      left = true;
-      activeTab == 0 ? click(lastTab) : click(activeTab - 1);
-    }
-    if (rtl) {
-      left = !left;
-    }
-    xDown = yDown = xDiff = yDiff = null;
-  }
-
-  function filterTabs() {
-    if (skip_hidden) {
-      tabs = tabs.filter(element => {
-        return (
-          !skip_tabs.includes(tabs.indexOf(element)) &&
-          getComputedStyle(element, null).display != "none"
-        );
-      });
-    } else {
-      tabs = tabs.filter(element => {
-        return !skip_tabs.includes(tabs.indexOf(element));
-      });
-    }
-    firstTab = wrap ? 0 : null;
-    lastTab = wrap ? tabs.length - 1 : null;
-  }
-
-  function click(index) {
-    if (
-      (activeTab == 0 && !wrap && left) ||
-      (activeTab == tabs.length - 1 && !wrap && !left)
-    ) {
-      return;
-    }
-    if (animate == "swipe") {
-      let _in = left ? `${screen.width / 1.5}px` : `-${screen.width / 1.5}px`;
-      let _out = left ? `-${screen.width / 1.5}px` : `${screen.width / 1.5}px`;
-      view.style.transitionDuration = "200ms";
-      view.style.opacity = 0;
-      view.style.transform = `translate(${_in}, 0)`;
-      view.style.transition = "transform 0.20s, opacity 0.18s";
-      setTimeout(function() {
-        tabs[index].dispatchEvent(
-          new MouseEvent("click", { bubbles: false, cancelable: true })
-        );
-        view.style.transitionDuration = "0ms";
-        view.style.transform = `translate(${_out}, 0)`;
-        view.style.transition = "transform 0s";
-      }, 210);
-      setTimeout(function() {
-        view.style.transitionDuration = "200ms";
-        view.style.opacity = 1;
-        view.style.transform = `translate(0px, 0)`;
-        view.style.transition = "transform 0.20s, opacity 0.18s";
-      }, 250);
-    } else if (animate == "fade") {
-      view.style.transitionDuration = "200ms";
-      view.style.transition = "opacity 0.20s";
-      view.style.opacity = 0;
-      setTimeout(function() {
-        tabs[index].dispatchEvent(
-          new MouseEvent("click", { bubbles: false, cancelable: true })
-        );
-        view.style.transitionDuration = "0ms";
-        view.style.opacity = 0;
-        view.style.transition = "opacity 0s";
-      }, 210);
-      setTimeout(function() {
-        view.style.transitionDuration = "200ms";
-        view.style.transition = "opacity 0.20s";
-        view.style.opacity = 1;
-      }, 250);
-    } else if (animate == "flip") {
-      view.style.transitionDuration = "200ms";
-      view.style.transform = "rotatey(90deg)";
-      view.style.transition = "transform 0.20s, opacity 0.20s";
-      view.style.opacity = 0.25;
-      setTimeout(function() {
-        tabs[index].dispatchEvent(
-          new MouseEvent("click", { bubbles: false, cancelable: true })
-        );
-      }, 210);
-      setTimeout(function() {
-        view.style.transitionDuration = "200ms";
-        view.style.transform = "rotatey(0deg)";
-        view.style.transition = "transform 0.20s, opacity 0.20s";
-        view.style.opacity = 1;
-      }, 250);
-    } else {
-      tabs[index].dispatchEvent(
-        new MouseEvent("click", { bubbles: false, cancelable: true })
-      );
-    }
-  }
-}
-
-const callback = mutations => {
-  mutations.forEach(({ addedNodes }) => {
-    for (const node of addedNodes) {
-      if (node.nodeName == "HA-PANEL-LOVELACE") {
-        swipeNavigation();
-      }
-    }
-  });
-};
-
-const dashboard_observer = new MutationObserver(callback);
-dashboard_observer.observe(
-  document
-    .querySelector("home-assistant")
-    .shadowRoot.querySelector("home-assistant-main")
-    .shadowRoot.querySelector("partial-panel-resolver"),
-  { childList: true }
-);
-
-swipeNavigation();
+"use strict";function A(t,e){var n;if("undefined"==typeof Symbol||null==t[Symbol.iterator]){if(Array.isArray(t)||(n=s(t)
+)||e&&t&&"number"==typeof t.length){n&&(t=n);var o=0,e=function(){};return{s:e,n:function(){return o>=t.length?{done:!0}:{done:!1,value:t[o++]}},
+e:function(t){throw t},f:e}}throw new TypeError(
+"Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}var r,a=!0,
+i=!1;return{s:function(){n=t[Symbol.iterator]()},n:function(){var t=n.next();return a=t.done,t},e:function(t){i=!0,r=t},f:function(){try{
+a||null==n.return||n.return()}finally{if(i)throw r}}}}function s(t,e){if(t){if("string"==typeof t)return o(t,e);var n=Object.prototype.toString.call(t
+).slice(8,-1);return"Object"===n&&t.constructor&&(n=t.constructor.name),"Map"===n||"Set"===n?Array.from(t
+):"Arguments"===n||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)?o(t,e):void 0}}function o(t,e){(null==e||e>t.length)&&(e=t.length);for(var n=0,
+o=new Array(e);n<e;n++)o[n]=t[n];return o}function E(t){return(E="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(t){
+return typeof t}:function(t){return t&&"function"==typeof Symbol&&t.constructor===Symbol&&t!==Symbol.prototype?"symbol":typeof t})(t)}function g(){
+var t,e,o,r,n,a,i,s,l,c,u,y,f,d,p,m,h,v,b,w=document.querySelector("home-assistant");function S(t){var e,n;0==p&&!r&&v||p==b.length-1&&!r&&!v||(
+"swipe"==o?(e=(v?"":"-").concat(screen.width/1.5,"px"),n=(v?"-":"").concat(screen.width/1.5,"px"),l.style.transitionDuration="200ms",l.style.opacity=0
+,l.style.transform="translate(".concat(e,", 0)"),l.style.transition="transform 0.20s, opacity 0.18s",setTimeout(function(){b[t].dispatchEvent(
+new MouseEvent("click",{bubbles:!1,cancelable:!0})),l.style.transitionDuration="0ms",l.style.transform="translate(".concat(n,", 0)"),
+l.style.transition="transform 0s"},210),setTimeout(function(){l.style.transitionDuration="200ms",l.style.opacity=1,
+l.style.transform="translate(0px, 0)",l.style.transition="transform 0.20s, opacity 0.18s"},250)):"fade"==o?(l.style.transitionDuration="200ms",
+l.style.transition="opacity 0.20s",l.style.opacity=0,setTimeout(function(){b[t].dispatchEvent(new MouseEvent("click",{bubbles:!1,cancelable:!0})),
+l.style.transitionDuration="0ms",l.style.opacity=0,l.style.transition="opacity 0s"},210),setTimeout(function(){l.style.transitionDuration="200ms",
+l.style.transition="opacity 0.20s",l.style.opacity=1},250)):"flip"==o?(l.style.transitionDuration="200ms",l.style.transform="rotatey(90deg)",
+l.style.transition="transform 0.20s, opacity 0.20s",l.style.opacity=.25,setTimeout(function(){b[t].dispatchEvent(new MouseEvent("click",{bubbles:!1,
+cancelable:!0}))},210),setTimeout(function(){l.style.transitionDuration="200ms",l.style.transform="rotatey(0deg)",
+l.style.transition="transform 0.20s, opacity 0.20s",l.style.opacity=1},250)):b[t].dispatchEvent(new MouseEvent("click",{bubbles:!1,cancelable:!0})))}
+null!=(w=(w=(w=(w=(w=(w=(w=(w=w&&w.shadowRoot)&&w.querySelector("home-assistant-main"))&&w.shadowRoot)&&w.querySelector(
+"app-drawer-layout partial-panel-resolver"))&&w.shadowRoot||w)&&w.querySelector("ha-panel-lovelace"))&&w.shadowRoot)&&w.querySelector("hui-root"))?(
+t=w.lovelace.config.swipe_nav||{},
+e=w.lovelace.config.custom_header&&w.lovelace.config.custom_header.reverse_tab_direction||"rtl"==document.querySelector("home-assistant"
+).style.direction,o=void 0!==t.animate?t.animate:"none",r=void 0===t.wrap||t.wrap,n=void 0!==t.prevent_default&&t.prevent_default,
+a=void 0!==t.swipe_amount?t.swipe_amount/Math.pow(10,2):.15,i=void 0===t.skip_hidden||t.skip_hidden,s=void 0!==t.skip_tabs?String(t.skip_tabs
+).replace(/\s+/g,"").split(",").map(function(t){return parseInt(t,10)}):[],w=w.shadowRoot.querySelector("ha-app-layout"),l=w.querySelector(
+'[id="view"]'),c=w.querySelector("paper-tabs")||w.querySelector("ha-tabs"),b=c?Array.from(c.querySelectorAll("paper-tab")):[],c&&(w.addEventListener(
+"touchstart",function(t){var e=["APP-HEADER","HA-SLIDER","SWIPE-CARD","HUI-MAP-CARD","ROUND-SLIDER","XIAOMI-VACUUM-MAP-CARD","HA-SIDEBAR"];if(
+"object"==E(t.path)){var n=A(t.path);try{for(n.s();!(o=n.n()).done;){var o=o.value;if("HUI-VIEW"==o.nodeName)break;if(-1<e.indexOf(o.nodeName))return}
+}catch(t){n.e(t)}finally{n.f()}}u=t.touches[0].clientX,y=t.touches[0].clientY,h||function(){b=i?b.filter(function(t){return!s.includes(b.indexOf(t)
+)&&"none"!=getComputedStyle(t,null).display}):b.filter(function(t){return!s.includes(b.indexOf(t))});m=r?0:null,h=r?b.length-1:null}();p=b.indexOf(
+c.querySelector(".iron-selected"))},{passive:!0}),w.addEventListener("touchmove",function(t){u&&y&&(f=u-t.touches[0].clientX,d=y-t.touches[0].clientY,
+Math.abs(f)>Math.abs(d)&&n&&t.preventDefault())},{passive:!1}),w.addEventListener("touchend",function(){if(p<0||Math.abs(f)<Math.abs(d))return void(
+u=y=f=d=null);e&&(f=-f);f>Math.abs(screen.width*a)?(v=!1,p==b.length-1?S(m):S(p+1)):f<-Math.abs(screen.width*a)&&(v=!0,S(0==p?h:p-1));e&&(v=!v)
+;u=y=f=d=null},{passive:!0})),"swipe"==o&&(w.style.overflow="hidden")):setTimeout(g,300)}var t=function(t){t.forEach(function(t){var e,n=A(
+t.addedNodes);try{for(n.s();!(e=n.n()).done;)"HA-PANEL-LOVELACE"==e.value.nodeName&&g()}catch(t){n.e(t)}finally{n.f()}})},e=new MutationObserver(t)
+;e.observe(document.querySelector("home-assistant").shadowRoot.querySelector("home-assistant-main").shadowRoot.querySelector("partial-panel-resolver")
+,{childList:!0}),g();
