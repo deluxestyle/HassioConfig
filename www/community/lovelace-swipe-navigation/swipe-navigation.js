90474e5544466957d855b432017029553a4668cf
deluxestyle <deluxestyle@googlemail.com>
2021-01-09 18:03:57 +0100

    update custom component

--- www/community/lovelace-swipe-navigation/swipe-navigation.js
+++ www/community/lovelace-swipe-navigation/swipe-navigation.js
@@ -1,57 +1,51 @@
-"use strict";function t(t,e){return r(t)||o(t,e)||s(t,e)||n()}function n(){throw new TypeError(
+"use strict";function t(t,e){return o(t)||r(t,e)||s(t,e)||n()}function n(){throw new TypeError(
 "Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}
-function o(t,e){if("undefined"!=typeof Symbol&&Symbol.iterator in Object(t)){var n=[],o=!0,r=!1,a=void 0;try{for(var i,l=t[Symbol.iterator]();!(o=(
-i=l.next()).done)&&(n.push(i.value),!e||n.length!==e);o=!0);}catch(t){r=!0,a=t}finally{try{o||null==l.return||l.return()}finally{if(r)throw a}}
-return n}}function r(t){if(Array.isArray(t))return t}function e(t){return l(t)||i(t)||s(t)||a()}function a(){throw new TypeError(
+function r(t,e){if("undefined"!=typeof Symbol&&Symbol.iterator in Object(t)){var n=[],r=!0,o=!1,a=void 0;try{for(var i,l=t[Symbol.iterator]();!(r=(
+i=l.next()).done)&&(n.push(i.value),!e||n.length!==e);r=!0);}catch(t){o=!0,a=t}finally{try{r||null==l.return||l.return()}finally{if(o)throw a}}
+return n}}function o(t){if(Array.isArray(t))return t}function e(t){return l(t)||i(t)||s(t)||a()}function a(){throw new TypeError(
 "Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}function i(t){
-if("undefined"!=typeof Symbol&&Symbol.iterator in Object(t))return Array.from(t)}function l(t){if(Array.isArray(t))return c(t)}function S(t,e){var n
-;if("undefined"==typeof Symbol||null==t[Symbol.iterator]){if(Array.isArray(t)||(n=s(t))||e&&t&&"number"==typeof t.length){n&&(t=n);var o=0,e=function(
-){};return{s:e,n:function(){return o>=t.length?{done:!0}:{done:!1,value:t[o++]}},e:function(t){throw t},f:e}}throw new TypeError(
-"Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}var r,a=!0,
-i=!1;return{s:function(){n=t[Symbol.iterator]()},n:function(){var t=n.next();return a=t.done,t},e:function(t){i=!0,r=t},f:function(){try{
-a||null==n.return||n.return()}finally{if(i)throw r}}}}function s(t,e){if(t){if("string"==typeof t)return c(t,e);var n=Object.prototype.toString.call(t
+if("undefined"!=typeof Symbol&&Symbol.iterator in Object(t))return Array.from(t)}function l(t){if(Array.isArray(t))return c(t)}function v(t,e){var n
+;if("undefined"==typeof Symbol||null==t[Symbol.iterator]){if(Array.isArray(t)||(n=s(t))||e&&t&&"number"==typeof t.length){n&&(t=n);var r=0,e=function(
+){};return{s:e,n:function(){return r>=t.length?{done:!0}:{done:!1,value:t[r++]}},e:function(t){throw t},f:e}}throw new TypeError(
+"Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}var o,a=!0,
+i=!1;return{s:function(){n=t[Symbol.iterator]()},n:function(){var t=n.next();return a=t.done,t},e:function(t){i=!0,o=t},f:function(){try{
+a||null==n.return||n.return()}finally{if(i)throw o}}}}function s(t,e){if(t){if("string"==typeof t)return c(t,e);var n=Object.prototype.toString.call(t
 ).slice(8,-1);return"Object"===n&&t.constructor&&(n=t.constructor.name),"Map"===n||"Set"===n?Array.from(t
 ):"Arguments"===n||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)?c(t,e):void 0}}function c(t,e){(null==e||e>t.length)&&(e=t.length);for(var n=0,
-o=new Array(e);n<e;n++)o[n]=t[n];return o}function g(t){return(g="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(t){
-return typeof t}:function(t){return t&&"function"==typeof Symbol&&t.constructor===Symbol&&t!==Symbol.prototype?"symbol":typeof t})(t)}var A={},x=0
-;function E(){if(x<10)try{A.ha=document.querySelector("home-assistant"),A.main=A.ha.shadowRoot.querySelector("home-assistant-main").shadowRoot,
-A.panel=A.main.querySelector("partial-panel-resolver"),A.ll=A.main.querySelector("ha-panel-lovelace"),A.root=A.ll.shadowRoot.querySelector("hui-root")
-,A.appLayout=A.root.shadowRoot.querySelector("ha-app-layout")}catch(t){x++,setTimeout(function(){return E()},50)}else A.ll&&!A.root?console.log(
-"hui-root not found."):A.ll&&!A.appLayout&&console.log("ha-app-layout not found.")}var M=["APP-HEADER","HA-SLIDER","SWIPE-CARD","HUI-MAP-CARD",
-"ROUND-SLIDER","XIAOMI-VACUUM-MAP-CARD","HA-SIDEBAR"];function L(){if(document.querySelector("home-assistant").shadowRoot.querySelector(
-"home-assistant-main").shadowRoot.querySelector("ha-panel-lovelace")){if(!A.appLayout||!A.appLayout.isConnected)return 0==x?(E(),void L()):void 0
-;var o,r,e,n,a,i,l,s,c=A.appLayout.querySelector('[id="view"]'),u=A.appLayout.querySelector("paper-tabs")||A.appLayout.querySelector("ha-tabs"),
-y=u?Array.from(u.querySelectorAll("paper-tab")):[],t="rtl"==A.ha.style.direction,f=A.ll.lovelace.config.swipe_nav||{},
-p=null!=f.animate?f.animate:"none",d=null==f.wrap||f.wrap,h=null!=f.prevent_default&&f.prevent_default,v=null!=f.swipe_amount?f.swipe_amount/Math.pow(
-10,2):.15,m=null==f.skip_hidden||f.skip_hidden,b=null!=f.skip_tabs?String(f.skip_tabs).replace(/\s+/g,"").split(",").map(function(t){return parseInt(t
-,10)}):[];u&&(A.appLayout.addEventListener("touchstart",function(t){if("object"==g(t.path)){var e=S(t.path);try{for(e.s();!(n=e.n()).done;){
-var n=n.value;if("HUI-VIEW"==n.nodeName)break;if(-1<M.indexOf(n.nodeName))return}}catch(t){e.e(t)}finally{e.f()}}o=t.touches[0].clientX,
-r=t.touches[0].clientY,l||(y=m?y.filter(function(t){return!b.includes(y.indexOf(t))&&"none"!=getComputedStyle(t,null).display}):y.filter(function(t){
-return!b.includes(y.indexOf(t))}),i=d?0:null,l=d?y.length-1:null);a=y.indexOf(u.querySelector(".iron-selected"))},{passive:!0}),
-A.appLayout.addEventListener("touchmove",function(t){o&&r&&(e=o-t.touches[0].clientX,n=r-t.touches[0].clientY,Math.abs(e)>Math.abs(n
-)&&h&&t.preventDefault())},{passive:!1}),A.appLayout.addEventListener("touchend",function(){if(a<0||Math.abs(e)<Math.abs(n))return void(o=r=e=n=null)
-;t&&(e=-e);e>Math.abs(screen.width*v)?(s=!1,a==y.length-1?w(i):w(a+1)):e<-Math.abs(screen.width*v)&&(s=!0,w(0==a?l:a-1));t&&(s=!s);o=r=e=n=null},{
-passive:!0}),"swipe"==p&&(A.appLayout.style.overflow="hidden"))}function w(t){var e,n;0==a&&!d&&s||a==y.length-1&&!d&&!s||("swipe"==p?(e=(s?"":"-"
-).concat(screen.width/1.5,"px"),n=(s?"-":"").concat(screen.width/1.5,"px"),c.style.transitionDuration="200ms",c.style.opacity=0,
-c.style.transform="translate(".concat(e,", 0)"),c.style.transition="transform 0.20s, opacity 0.18s",setTimeout(function(){y[t].dispatchEvent(
-new MouseEvent("click",{bubbles:!1,cancelable:!0})),c.style.transitionDuration="0ms",c.style.transform="translate(".concat(n,", 0)"),
-c.style.transition="transform 0s"},210),setTimeout(function(){c.style.transitionDuration="200ms",c.style.opacity=1,
-c.style.transform="translate(0px, 0)",c.style.transition="transform 0.20s, opacity 0.18s"},250)):"fade"==p?(c.style.transitionDuration="200ms",
-c.style.transition="opacity 0.20s",c.style.opacity=0,setTimeout(function(){y[t].dispatchEvent(new MouseEvent("click",{bubbles:!1,cancelable:!0})),
-c.style.transitionDuration="0ms",c.style.opacity=0,c.style.transition="opacity 0s"},210),setTimeout(function(){c.style.transitionDuration="200ms",
-c.style.transition="opacity 0.20s",c.style.opacity=1},250)):"flip"==p?(c.style.transitionDuration="200ms",c.style.transform="rotatey(90deg)",
-c.style.transition="transform 0.20s, opacity 0.20s",c.style.opacity=.25,setTimeout(function(){y[t].dispatchEvent(new MouseEvent("click",{bubbles:!1,
-cancelable:!0}))},210),setTimeout(function(){c.style.transitionDuration="200ms",c.style.transform="rotatey(0deg)",
-c.style.transition="transform 0.20s, opacity 0.20s",c.style.opacity=1},250)):y[t].dispatchEvent(new MouseEvent("click",{bubbles:!1,cancelable:!0})))}}
-function u(t){var e,n=S(t);try{for(n.s();!(e=n.n()).done;){var o=S(e.value.addedNodes);try{for(o.s();!(r=o.n()).done;){var r=r.value;if(
-"ha-panel-lovelace"==r.localName)return void new MutationObserver(y).observe(r.shadowRoot,{childList:!0})}}catch(t){o.e(t)}finally{o.f()}}}catch(t){
-n.e(t)}finally{n.f()}}function y(t){var e,n=S(t);try{for(n.s();!(e=n.n()).done;){var o=S(e.value.addedNodes);try{for(o.s();!(r=o.n()).done;){
-var r=r.value;if("hui-root"==r.localName)return void new MutationObserver(f).observe(r.shadowRoot,{childList:!0})}}catch(t){o.e(t)}finally{o.f()}}
-}catch(t){n.e(t)}finally{n.f()}}function f(t){var e,n=S(t);try{for(n.s();!(e=n.n()).done;){var o,r=S(e.value.addedNodes);try{for(r.s();!(o=r.n()
-).done;)if("ha-app-layout"==o.value.localName)return void L()}catch(t){r.e(t)}finally{r.f()}}}catch(t){n.e(t)}finally{n.f()}}E(),x=0,L(),
-new MutationObserver(u).observe(A.panel,{childList:!0});for(var p={header:"%c≡ swipe-navigation".padEnd(27),ver:"%cversion 1.3.3 "},d="%c\n",
-h=Math.max.apply(Math,e(Object.values(p).map(function(t){return t.length}))),v=0,m=Object.entries(p);v<m.length;v++){var b=t(m[v],1),w=b[0]
-;p[w].length<=h&&(p[w]=p[w].padEnd(h)),"header"==w&&(p[w]="".concat(p[w].slice(0,-1),"⋮ "))}
-var D="display:inline-block;border-width:1px 1px 0 1px;border-style:solid;border-color:#424242;color:white;background:#03a9f4;font-size:12px;padding:4px 4.5px 5px 6px;"
-,I="border-width:0px 1px 1px 1px;padding:7px;background:white;color:#424242;line-height:0.7;";console.info(p.header+d+p.ver,D,"","".concat(D," "
-).concat(I));
+r=new Array(e);n<e;n++)r[n]=t[n];return r}function w(t){return(w="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(t){
+return typeof t}:function(t){return t&&"function"==typeof Symbol&&t.constructor===Symbol&&t!==Symbol.prototype?"symbol":typeof t})(t)}var u,S,g,
+A=document.querySelector("home-assistant"),y=A.shadowRoot.querySelector("home-assistant-main").shadowRoot,f=y.querySelector("partial-panel-resolver"),
+x=0,E={},M=["APP-HEADER","HA-SLIDER","SWIPE-CARD","HUI-MAP-CARD","ROUND-SLIDER","XIAOMI-VACUUM-MAP-CARD","HA-SIDEBAR"];function p(){
+var t=y.querySelector("ha-panel-lovelace");t&&(E={},d(t))}function d(e){x++;try{var t=e.lovelace.config;E=t.swipe_nav||{},
+u=e.shadowRoot.querySelector("hui-root"),S=u.shadowRoot.querySelector("ha-app-layout"),g=S.querySelector('[id="view"]'),h()}catch(t){x<40&&setTimeout(
+function(){return d(e)},50)}}function h(){x=0;var r,o,e,n,a,i,l,s,c=S.querySelector("paper-tabs")||S.querySelector("ha-tabs"),u=c?Array.from(
+c.querySelectorAll("paper-tab")):[],t="rtl"==A.style.direction,y=null!=E.animate?E.animate:"none",f=null==E.wrap||E.wrap,
+p=null!=E.prevent_default&&E.prevent_default,d=null!=E.swipe_amount?E.swipe_amount/Math.pow(10,2):.15,h=null==E.skip_hidden||E.skip_hidden,
+b=null!=E.skip_tabs?String(E.skip_tabs).replace(/\s+/g,"").split(",").map(function(t){return parseInt(t,10)}):[];function m(t){var e,n
+;0==a&&!f&&s||a==u.length-1&&!f&&!s||("swipe"==y?(e=(s?"":"-").concat(screen.width/1.5,"px"),n=(s?"-":"").concat(screen.width/1.5,"px"),
+g.style.transitionDuration="200ms",g.style.opacity=0,g.style.transform="translate(".concat(e,", 0)"),
+g.style.transition="transform 0.20s, opacity 0.18s",setTimeout(function(){u[t].dispatchEvent(new MouseEvent("click",{bubbles:!1,cancelable:!0})),
+g.style.transitionDuration="0ms",g.style.transform="translate(".concat(n,", 0)"),g.style.transition="transform 0s"},210),setTimeout(function(){
+g.style.transitionDuration="200ms",g.style.opacity=1,g.style.transform="translate(0px, 0)",g.style.transition="transform 0.20s, opacity 0.18s"},250)
+):"fade"==y?(g.style.transitionDuration="200ms",g.style.transition="opacity 0.20s",g.style.opacity=0,setTimeout(function(){u[t].dispatchEvent(
+new MouseEvent("click",{bubbles:!1,cancelable:!0})),g.style.transitionDuration="0ms",g.style.opacity=0,g.style.transition="opacity 0s"},210),
+setTimeout(function(){g.style.transitionDuration="200ms",g.style.transition="opacity 0.20s",g.style.opacity=1},250)):"flip"==y?(
+g.style.transitionDuration="200ms",g.style.transform="rotatey(90deg)",g.style.transition="transform 0.20s, opacity 0.20s",g.style.opacity=.25,
+setTimeout(function(){u[t].dispatchEvent(new MouseEvent("click",{bubbles:!1,cancelable:!0}))},210),setTimeout(function(){
+g.style.transitionDuration="200ms",g.style.transform="rotatey(0deg)",g.style.transition="transform 0.20s, opacity 0.20s",g.style.opacity=1},250)
+):u[t].dispatchEvent(new MouseEvent("click",{bubbles:!1,cancelable:!0})))}c&&(S.addEventListener("touchstart",function(t){if("object"==w(t.path)){
+var e=v(t.path);try{for(e.s();!(n=e.n()).done;){var n=n.value;if("HUI-VIEW"==n.nodeName)break;if(-1<M.indexOf(n.nodeName))return}}catch(t){e.e(t)
+}finally{e.f()}}r=t.touches[0].clientX,o=t.touches[0].clientY,l||(u=h?u.filter(function(t){return!b.includes(u.indexOf(t))&&"none"!=getComputedStyle(t
+,null).display}):u.filter(function(t){return!b.includes(u.indexOf(t))}),i=f?0:null,l=f?u.length-1:null);a=u.indexOf(c.querySelector(".iron-selected"))
+},{passive:!0}),S.addEventListener("touchmove",function(t){r&&o&&(e=r-t.touches[0].clientX,n=o-t.touches[0].clientY,Math.abs(e)>Math.abs(n
+)&&p&&t.preventDefault())},{passive:!1}),S.addEventListener("touchend",function(){if(a<0||Math.abs(e)<Math.abs(n))return void(r=o=e=n=null);t&&(e=-e)
+;e>Math.abs(screen.width*d)?(s=!1,a==u.length-1?m(i):m(a+1)):e<-Math.abs(screen.width*d)&&(s=!0,m(0==a?l:a-1));t&&(s=!s);r=o=e=n=null},{passive:!0}),
+"swipe"==y&&(S.style.overflow="hidden"))}function b(t){I(t,"ha-panel-lovelace",m)}function m(t){I(t,"hui-root",D)}function D(t){I(t,"ha-app-layout",
+null)}function I(t,e,n){var r,o=v(t);try{for(o.s();!(r=o.n()).done;){var a=v(r.value.addedNodes);try{for(a.s();!(i=a.n()).done;){var i=i.value;if(
+i.localName==e)return void(n?new MutationObserver(n).observe(i.shadowRoot,{childList:!0}):p())}}catch(t){a.e(t)}finally{a.f()}}}catch(t){o.e(t)
+}finally{o.f()}}p(),new MutationObserver(b).observe(f,{childList:!0});for(var O={header:"%c≡ swipe-navigation".padEnd(27),ver:"%cversion 1.3.5 "},
+R="%c\n",k=Math.max.apply(Math,e(Object.values(O).map(function(t){return t.length}))),q=0,j=Object.entries(O);q<j.length;q++){var T=t(j[q],1),_=T[0]
+;O[_].length<=k&&(O[_]=O[_].padEnd(k)),"header"==_&&(O[_]="".concat(O[_].slice(0,-1),"⋮ "))}
+var L="display:inline-block;border-width:1px 1px 0 1px;border-style:solid;border-color:#424242;color:white;background:#03a9f4;font-size:12px;padding:4px 4.5px 5px 6px;"
+,C="border-width:0px 1px 1px 1px;padding:7px;background:white;color:#424242;line-height:0.7;";console.info(O.header+R+O.ver,L,"","".concat(L," "
+).concat(C));
