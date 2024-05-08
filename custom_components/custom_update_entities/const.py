b56c727bac563b84fd203c7c742f87951eb0f6c4
deluxestyle <deluxestyle@googlemail.com>
2024-05-08 12:51:39 +0200

    updates

--- custom_components/custom_update_entities/const.py
+++
@@ -1,8 +0,0 @@
-DOMAIN = "custom_update_entities"
-CONF_UPDATERS = "updaters"
-CONF_NAME = "name"
-CONF_LATEST_VERSION_ENTITY = "latest_version_entity"
-CONF_INSTALLED_VERSION_ENTITY = "installed_version_entity"
-CONF_UPDATE_ACTION = "update_action"
-CONF_RELEASE_NOTES_URL = "release_notes_url"
-CONF_LOGO_URL = "logo_url"
