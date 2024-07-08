82a15784fc8b6b8726df635e9ba37c9b17e1a633
deluxestyle <deluxestyle@googlemail.com>
2024-07-08 12:07:37 +0200

    clean up

--- python_scripts/dismiss_notifications.py
+++
@@ -1,4 +0,0 @@
-for notif in hass.states.entity_ids('persistent_notification'):
-    hass.services.call(
-        'persistent_notification', 'dismiss',
-        {"notification_id": notif.split('.', 1)[-1]})
